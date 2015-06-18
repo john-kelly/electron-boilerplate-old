224c01522e5b28647af985dec53e2139952a600e
john kelly <johnkelly@codehs.com>
2015-06-17 17:52:26 -0700

    clean up env_config vendor util

--- src/vendor/electron_boilerplate/env_config.js
+++ src/vendor/electron_boilerplate/env_config.js
@@ -1,15 +1,5 @@
-// Loads config/env_XXX.json file and puts it
-// in proper place for given Electron context.
-
 'use strict';
 
-(function() {
-    var jetpack = require('fs-jetpack');
-    if (typeof window === 'object') {
-        // Web browser context, __dirname points to dir of index.html
-        window.env = jetpack.read(__dirname + '/env_config.json', 'json');
-    } else {
-        // Node context
-        module.exports = jetpack.read(__dirname + '/../../env_config.json', 'json');
-    }
-}());
+// Loads config/env_XXX.json file and puts it
+// in proper place for given Electron context.
+module.exports = require('../../env_config.json');

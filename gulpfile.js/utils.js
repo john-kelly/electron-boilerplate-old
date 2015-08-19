3c5c33d9f1d04e43e8f8663b5ba0d09b6c213ad6
john kelly <johnkelly@codehs.com>
2015-08-19 00:07:37 -0700

    remove dead code

--- gulpfile.js/utils.js
+++ gulpfile.js/utils.js
@@ -16,14 +16,6 @@ module.exports.os = function() {
     return 'unsupported';
 };
 
-module.exports.replace = function(str, patterns) {
-    Object.keys(patterns).forEach(function(pattern) {
-        var matcher = new RegExp('{{' + pattern + '}}', 'g');
-        str = str.replace(matcher, patterns[pattern]);
-    });
-    return str;
-};
-
 module.exports.getEnvName = function() {
     return argv.env || 'development';
 };
